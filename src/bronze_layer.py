fce618b8855562bdeabcbfd923441811e6e14c58
elaoulamouhsine <elaoulamouhsine@gmail.com>
2026-03-14 15:35:37 +0000

    organise bronze layer script

--- src/bronze_layer.py
+++ src/bronze_layer.py
@@ -1,16 +1,23 @@
 from pyspark.sql.functions import current_timestamp, lit
 
-raw_data_path = "/Volumes/workspace/restaurantdb/data"
 bronze_db_name = "workspace.bronze_restaurant"
-source_db = "workspace.restaurantdb"
-tables_to_move = ["order_details", "menu", "employees", "orders", "weather"]
-
 spark.sql(f"CREATE DATABASE IF NOT EXISTS {bronze_db_name}")
 
 def ingest_to_bronze(source_db, table_name, target_schema):
     """
-    Ingests data from an existing Databricks table into the Bronze layer 
-    with added metadata for auditing.
+    Ingests a source table into the Bronze layer with auditing metadata.
+
+    Reads a specified table from the source database using the active Spark session, 
+    appends 'ingestion_timestamp' and 'source_system' columns for tracking, and saves 
+    the result as a Delta table in the target schema using overwrite mode.
+
+    Args:
+        source_db (str): The name of the source database containing the raw table.
+        table_name (str): The name of the table to be ingested.
+        target_schema (str): The target database schema for the Bronze layer.
+
+    Returns:
+        None
     """
     print(f"Ingesting {source_db}.{table_name} into {target_schema}.{table_name}.")
     
@@ -26,5 +33,7 @@ def ingest_to_bronze(source_db, table_name, target_schema):
     
     print(f"Successfully loaded {table_name}.")
 
+source_db = "workspace.restaurantdb"
+tables_to_move = ["order_details", "menu", "employees", "orders", "weather"]
 for t in tables_to_move:
     ingest_to_bronze(source_db, t, bronze_db_name)
