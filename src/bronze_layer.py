158a793c6e8511580c8fb839d6b26eac23fdc90b
elaoulamouhsine <elaoulamouhsine@gmail.com>
2026-03-14 15:09:44 +0000

    refine initialise_db notebook and bronze layer script

--- src/bronze_layer.py
+++ src/bronze_layer.py
@@ -1,41 +1,30 @@
 from pyspark.sql.functions import current_timestamp, lit
 
 raw_data_path = "/Volumes/workspace/restaurantdb/data"
-bronze_db_name = "bronze_restaurant"
+bronze_db_name = "workspace.bronze_restaurant"
+source_db = "workspace.restaurantdb"
+tables_to_move = ["order_details", "menu", "employees", "orders", "weather"]
 
 spark.sql(f"CREATE DATABASE IF NOT EXISTS {bronze_db_name}")
 
-
-def ingest_to_bronze(file_name, table_name):
-    print(f"Ingesting {file_name} into {bronze_db_name}.{table_name}.")
+def ingest_to_bronze(source_db, table_name, target_schema):
+    """
+    Ingests data from an existing Databricks table into the Bronze layer 
+    with added metadata for auditing.
+    """
+    print(f"Ingesting {source_db}.{table_name} into {target_schema}.{table_name}.")
     
-    # Read CSV
-    df_raw = (spark.read
-              .format("csv")
-              .option("header", "true")
-              .option("inferSchema", "true")
-              .load(f"{raw_data_path}/{file_name}"))
+    df_raw = spark.table(f"{source_db}.{table_name}")
     
-    # Add metadata: ingestion time and source file
     df_bronze = df_raw.withColumn("ingestion_timestamp", current_timestamp()) \
-                      .withColumn("source_file", lit(file_name))
+                      .withColumn("source_system", lit(source_db))
     
-    # Write to Delta
-    (df_bronze.write
-     .format("delta")
-     .mode("overwrite") # Use "append" for daily batches
-     .option("overwriteSchema", "true")
-     .saveAsTable(f"{bronze_db_name}.{table_name}"))
+    df_bronze.write.format("delta") \
+              .mode("overwrite") \
+              .option("overwriteSchema", "true") \
+              .saveAsTable(f"{target_schema}.{table_name}")
     
-    print(f"Table {table_name} updated successfully.\n")
+    print(f"Successfully loaded {table_name}.")
 
-tables_to_ingest = [
-    ("menu_raw.csv", "menu"),
-    ("employees_raw.csv", "employees"),
-    ("weather_raw.csv", "weather"),
-    ("orders_raw.csv", "orders"),
-    ("details_raw.csv", "order_details")
-]
-
-for csv_file, delta_table in tables_to_ingest:
-    ingest_to_bronze(csv_file, delta_table)
+for t in tables_to_move:
+    ingest_to_bronze(source_db, t, bronze_db_name)
